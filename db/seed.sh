881568d7565d28d0cc81f682550a33043f7ae363
Aquila-Inacio <134939594+Aquila-Inacio@users.noreply.github.com>
2024-10-01 15:34:34 -0300

    tirando o localhost seed.sh

--- db/seed.sh
+++ db/seed.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo "Wait for postgreSQL database start..." >> /dev/stdout
-time wait-for localhost:5432 -t 300 -- echo "PostgreSQL ready!" >> /dev/stdout
+time wait-for db:5432 -t 300 -- echo "PostgreSQL ready!" >> /dev/stdout
 sleep 3
-createdb -h localhost -U postgres -e reservation  >> /dev/stdout
-pg_restore -h localhost -U postgres -d reservation < pgdb.psql  >> /dev/stdout
+createdb -h db -U postgres -e reservation  >> /dev/stdout
+pg_restore -h db -U postgres -d reservation < pgdb.psql  >> /dev/stdout
 rm -rf async-seed.sh seed.sh
